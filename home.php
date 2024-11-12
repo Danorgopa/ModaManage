6097a1d639f14e86b4384bf5485f26ccdaf69d80
Danorgopa <bydanorgopa@gmail.com>
2024-11-12 13:33:00 -0500

    UI 1.3
    coreccion de sidebar

--- home.php
+++ home.php
@@ -31,8 +31,8 @@ if (!isset($_SESSION['username'])) {
                 <li><a href="perfil.php"><i class="fas fa-user"></i> Perfil</a></li> <!-- Enlace al módulo de perfil -->
                 <li><a href="home.php"><i class="fas fa-home"></i> Dashboard</a></li>
                 <li><a href="inventario.php"><i class="fas fa-boxes"></i> Inventario</a></li>
-                <li><a href="mantenimiento.php"><i class="fas fa-tools"></i> Mantenimiento</a></li>
-                <li><a href="generar_reporte.php"><i class="fas fa-chart-line"></i> Reportes</a></li>
+                <li><a href="mantenimiento.php"><i class="fas fa-tools"></i> Empleados</a></li>
+                <li><a href="generar_reporte.php"><i class="fas fa-chart-line"></i> Proveedores</a></li>
                 <li><a href="configuracion.php"><i class="fas fa-cog"></i> Informacion</a></li>
                 <li><a href="logout.php"><i class="fas fa-sign-out-alt"></i> Cerrar Sesión</a></li>
             </ul>
--- perfil.php
+++ perfil.php
@@ -74,8 +74,8 @@ $conn->close();
                 <li><a href="perfil.php"><i class="fas fa-user"></i> Perfil</a></li>
                 <li><a href="home.php"><i class="fas fa-home"></i> Dashboard</a></li>
                 <li><a href="inventario.php"><i class="fas fa-boxes"></i> Inventario</a></li>
-                <li><a href="mantenimiento.php"><i class="fas fa-tools"></i> Mantenimiento</a></li>
-                <li><a href="reportes.php"><i class="fas fa-chart-line"></i> Reportes</a></li>
+                <li><a href="mantenimiento.php"><i class="fas fa-tools"></i> Empleados</a></li>
+                <li><a href="reportes.php"><i class="fas fa-chart-line"></i> Proveedores</a></li>
                 <li><a href="configuracion.php"><i class="fas fa-cog"></i> Informacion</a></li>
                 <li><a href="logout.php"><i class="fas fa-sign-out-alt"></i> Cerrar Sesión</a></li>
             </ul>
