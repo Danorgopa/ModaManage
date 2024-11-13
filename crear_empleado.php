ab164c481ccb16c90a5914f11bcb7380f435eada
Danorgopa <bydanorgopa@gmail.com>
2024-11-12 20:12:08 -0500

    UI 1.7

--- crear_empleado.php
+++ crear_empleado.php
@@ -104,8 +104,8 @@ $conn->close();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Añadir Nuevo Empleado</title>
+  
   <link rel="stylesheet" href="css/nuevos_dps2.css">
-  <link rel="stylesheet" href="css/general_sidebar.css">
   <style>
     .hidden {
       display: none;
